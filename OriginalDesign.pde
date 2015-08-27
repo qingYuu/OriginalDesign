1ade96cbcdf49ff091fe606679fa05b40566e452
unknown <xiyu5@697-334-Frodo.sf.k12.internal>
2015-08-27 11:37:43 -0700

    changed headphones

--- OriginalDesign.pde
+++ OriginalDesign.pde
@@ -1,21 +1,54 @@
 void setup()
 {
-size(512, 512);
-}
-void draw()
-{  
-outsideegg();
-insideegg();
+size(500,500);
 }
 
-void outsideegg()
+void draw()
+{
+headphone();
+KuruFace();
+Eyes();
+mouth();
+if (mousePressed==true) 
+	{  
+		angry();
+	}
+}
+
+void KuruFace()
+{
+fill(255,208,0);
+stroke(255,208,0);
+ellipse(200,200,200,200);
+}
+void Eyes()
 {
 fill(255,255,255);
-ellipse(300, 300, 100, 100);
+stroke(255,255,255);
+ellipse(150,175,70,70);
+ellipse(250,175,70,70);
 }
 
-void insideegg()
+void mouth()
 {
-fill(250,223,17);
-ellipse(300, 300, 30, 30);
+fill(255,255,255);
+stroke(255,255,255);
+
 }
+void headphone()
+{
+fill(255,255,255);
+stroke(255,255,255);
+ellipse(105,200,75,80);
+ellipse(295,200,75,80);
+}
+
+void angry()
+{
+fill(167,168,153,75);
+stroke(167,168,153,75);
+ellipse(200,175,85,95);
+}
+
+void mouth()
+
--- build-tmp/source/OriginalDesign.java
+++ build-tmp/source/OriginalDesign.java
@@ -16,25 +16,57 @@ public class OriginalDesign extends PApplet {
 
 public void setup()
 {
-size(512, 512);
-}
-public void draw()
-{  
-outsideegg();
-insideegg();
+size(500,500);
 }
 
-public void outsideegg()
+public void draw()
+{
+headphone();
+KuruFace();
+Eyes();
+mouth();
+if (mousePressed==true) 
+	{  
+		angry();
+	}
+}
+
+public void KuruFace()
+{
+fill(255,208,0);
+stroke(255,208,0);
+ellipse(200,200,200,200);
+}
+public void Eyes()
 {
 fill(255,255,255);
-ellipse(300, 300, 100, 100);
+stroke(255,255,255);
+ellipse(150,175,70,70);
+ellipse(250,175,70,70);
 }
 
-public void insideegg()
+public void mouth()
 {
-fill(250,223,17);
-ellipse(300, 300, 30, 30);
+fill(255,255,255);
+stroke(255,255,255);
+
 }
+public void headphone()
+{
+fill(255,255,255);
+stroke(255,255,255);
+ellipse(105,200,75,80);
+ellipse(295,200,75,80);
+}
+
+public void angry()
+{
+fill(167,168,153,75);
+stroke(167,168,153,75);
+ellipse(200,175,85,95);
+}
+
+
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "OriginalDesign" };
     if (passedArgs != null) {
